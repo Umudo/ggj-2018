c8ff4b5220ee4b75fccdb0dec884b625140c57e0
Unknown <bugrakaratatar@gmail.com>
2018-01-27 16:23:10 +0300

    Character Rotation script and Rotatable Object script

---
+++ Assets/Scripts/RotatableObject.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RotatableObject : MonoBehaviour {
+
+	void Start () {
+		
+	}
+	
+	void Update () {
+        
+    }
+
+    IEnumerator RotateMe(Vector3 byAngles, float inTime)
+    {
+        var fromAngle = transform.rotation;
+        var toAngle = Quaternion.Euler(transform.eulerAngles + byAngles);
+        for (var t = 0f; t < 1; t += Time.deltaTime / inTime)
+        {
+            transform.rotation = Quaternion.Lerp(fromAngle, toAngle, Mathf.SmoothStep(0.0f, 1.0f, Mathf.SmoothStep(0.0f, 1.0f, t)));
+            yield return null;
+        }
+    }
+
+    public void RotateObject(string rotation)
+    {
+        Vector3 direction;
+        if (rotation == "right")
+            direction = -Vector3.up;
+        else
+            direction = Vector3.up;
+        StartCoroutine(RotateMe(direction * 45, 1));
+    }
+}
---
+++ Assets/Scripts/Select.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Select : MonoBehaviour {
+    public RaycastHit hit;
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        Ray ray = Camera.main.ScreenPointToRay(new Vector3(Screen.width / 2, Screen.height / 2, 0));
+        if(Physics.Raycast(ray,out hit, 10) && hit.distance <= 1.5){
+            StartCoroutine(ShowRotateMessage());
+            if (hit.transform.gameObject.GetComponent<RotatableObject>() != null)
+            {
+
+                if (Input.GetKeyDown(KeyCode.E))
+                    hit.transform.gameObject.GetComponent<RotatableObject>().RotateObject("right");
+                if (Input.GetKeyDown(KeyCode.Q))
+                    hit.transform.gameObject.GetComponent<RotatableObject>().RotateObject("left");
+            }
+        }
+	}
+
+    IEnumerator ShowRotateMessage()
+    {
+        float delay = 1f;
+        GetComponent<GUIText>().text = "<-                                            ->";
+        GetComponent<GUIText>().enabled = true;
+        yield return new WaitForSeconds(delay);
+        GetComponent<GUIText>().enabled = false;
+    }
+}
