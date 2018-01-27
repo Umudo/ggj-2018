68a2711185a8609665404ba888f400468c6dbcd0
Unknown <bugrakaratatar@gmail.com>
2018-01-27 16:52:45 +0300

    rotate last

--- Assets/Scripts/CharacterRotateObject.cs
+++ Assets/Scripts/CharacterRotateObject.cs
@@ -2,7 +2,7 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Select : MonoBehaviour {
+public class CharacterRotateObject : MonoBehaviour {
     public RaycastHit hit;
 	// Use this for initialization
 	void Start () {
