378e27b5e0f0f2cc19e3ce6e423f5b492f8b26eb
Umudo <umutcang@gmail.com>
2018-01-27 21:12:59 +0300

    Add Key, Door and Lock Components.

---
+++ Assets/Scripts/Key.cs
@@ -0,0 +1,118 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEditor.UI;
+using UnityEngine;
+
+public class Key : MonoBehaviour
+{	
+	public bool isOn
+	{
+		get { return _isOn;  }
+		set
+		{
+			_isOn = value;
+			laserLineRenderer.enabled = value;
+		}
+	}
+
+	public LineRenderer laserLineRenderer;
+	public float laserWidth = 0.1f;
+	public float laserMaxLength = 5f;
+	
+	public GameObject relatedLock;
+
+	private const int _cameraRayDistance = 2;
+	private Camera _fpsCamera;
+	private bool _isOn;
+	private Lock _lock;
+
+	// Use this for initialization
+	void Start ()
+	{
+		_fpsCamera = Camera.main;
+
+		laserLineRenderer = GetComponent<LineRenderer>();
+		isOn = false;
+		
+		Vector3[] initLaserPositions = new Vector3[ 2 ] { Vector3.zero, Vector3.zero };
+		laserLineRenderer.SetPositions( initLaserPositions );
+		laserLineRenderer.startColor = Color.red;
+		laserLineRenderer.endColor = Color.red;
+		laserLineRenderer.startWidth = laserWidth;
+		laserLineRenderer.endWidth = laserWidth;
+		laserLineRenderer.enabled = isOn;
+		
+		if (relatedLock != null)
+		{
+			_lock = relatedLock.GetComponent<Lock>();
+		}
+		else
+		{
+			print("Related Lock is not set.");
+		}
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		castRayFromCamera();
+	}
+
+	void castRayFromCamera()
+	{
+		if (relatedLock == null)
+		{
+			print("Related Lock is not set.");
+			
+			return;
+		}
+		
+		RaycastHit hit;
+		Vector3 playerForward = _fpsCamera.transform.forward;
+		Debug.DrawRay(_fpsCamera.transform.position, playerForward * _cameraRayDistance, Color.green);
+
+		if (Physics.Raycast(_fpsCamera.transform.position, playerForward, out hit, _cameraRayDistance))
+		{
+			if (hit.collider.gameObject.name == this.gameObject.name)
+			{
+				if (Input.GetKeyDown(KeyCode.E))
+				{
+					isOn = !isOn;
+				}
+			}
+		}
+
+		if (this.isOn)
+		{
+			// If not already interacted, shoot the laser
+			if (!_lock.interactState)
+			{
+				ShootLaserFromTargetPosition( transform.position, transform.forward, laserMaxLength );
+			}
+		}
+	}
+	
+	void ShootLaserFromTargetPosition( Vector3 targetPosition, Vector3 direction, float length )
+	{
+		if (relatedLock == null)
+		{
+			print("Related Lock is not set.");
+			
+			return;
+		}
+		
+		Ray ray = new Ray( targetPosition, direction );
+		RaycastHit raycastHit;
+		Vector3 endPosition = targetPosition + ( length * direction );
+ 
+		if(Physics.Raycast(ray, out raycastHit, length)) {
+			endPosition = raycastHit.point;
+			if (raycastHit.collider.gameObject == relatedLock)
+			{
+				_lock.interactState = true;
+			}
+		}
+ 
+		laserLineRenderer.SetPosition( 0, targetPosition );
+		laserLineRenderer.SetPosition( 1, endPosition );
+	}
+}
---
+++ Assets/Scripts/Lock.cs
@@ -0,0 +1,53 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using Assets.Scripts.Interfaces;
+using UnityEngine;
+
+public class Lock : MonoBehaviour
+{
+
+	public GameObject interactableGameObject;
+
+	public bool interactState
+	{
+		get { return _interactState;}
+		set
+		{
+			_interactState = value;
+			if (_interactable != null)
+			{
+				_interactable.interact(value);
+			}
+		}
+	}
+
+	private bool _interactState;
+
+	private IInteractable _interactable;
+
+	// Use this for initialization
+	void Start ()
+	{
+		if (checkInteractableObjectImplementsInterface())
+		{
+			_interactable = interactableGameObject.GetComponent<IInteractable>();
+		}
+		else
+		{
+			print("Interactee Object Does Not Implement IInteractable");
+		}
+		
+		interactState = false;
+	}
+
+	bool checkInteractableObjectImplementsInterface()
+	{
+		var interacter = interactableGameObject.GetComponent<IInteractable>();
+		return interacter != null;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
