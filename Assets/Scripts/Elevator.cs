9d804810c95bbc5b8ce07cf0821163421175e927
Bahadir <bahadrix@gmail.com>
2018-01-27 14:19:39 +0300

    Auto stash before merge of "master" and "origin/master"

---
+++ Assets/Scripts/Elevator.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Elevator : MonoBehaviour {
+
+    private Animator _animator;
+
+	// Use this for initialization
+	void Start () {
+
+        _animator = GetComponent<Animator>();
+
+        toggle();
+	}
+	
+    public void toggle()
+    {
+        _animator.SetTrigger("toggle");
+    }
+
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
