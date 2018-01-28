6f4a8149665ec1ee41302f3b54f9ec3b82f61091
Umudo <umutcang@gmail.com>
2018-01-28 04:31:01 +0300

    Reformat code in Key.cs

--- Assets/Scripts/Key.cs
+++ Assets/Scripts/Key.cs
@@ -4,156 +4,157 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class Key : MonoBehaviour
-{	
-	public bool isOn
-	{
-		get { return _isOn;  }
-		set
-		{
-			_isOn = value;
-			if (_laserLineRenderer != null)
-			{
-				_laserLineRenderer.enabled = value;
-			}
-		}
-	}
+{
+    public bool isOn
+    {
+        get { return _isOn; }
+        set
+        {
+            _isOn = value;
+            if (_laserLineRenderer != null)
+            {
+                _laserLineRenderer.enabled = value;
+            }
+        }
+    }
 
-	public float laserMaxLength = 100000f; // This is dependent on the LaserHolder GameObject.
-	
-	public GameObject relatedLock;
+    public float laserMaxLength = 100000f; // This is dependent on the LaserHolder GameObject.
 
-	private LineRenderer _laserLineRenderer;
-	private GameObject _laserHolder;
-	
-	private const int _cameraRayDistance = 2;
-	private Camera _fpsCamera;
-	
-	private bool _isOn;
-	
-	private Lock _lock;
+    public GameObject relatedLock;
 
-	// Use this for initialization
-	void Start ()
-	{
-		_fpsCamera = Camera.main;
-		isOn = false;
+    private LineRenderer _laserLineRenderer;
+    private GameObject _laserHolder;
 
-		var childTransform = transform.Find("LaserHolder");
-		if (childTransform != null)
-		{
-			_laserHolder = childTransform.gameObject;
-			_laserLineRenderer = _laserHolder.GetComponent<LineRenderer>();
-			_laserLineRenderer.enabled = isOn;
-		}
-		else
-		{
-			throw new ArgumentException("Can not find a child gameObject named LaserHolder in Key object.");
-		}
-		
-		if (relatedLock != null)
-		{
-			_lock = relatedLock.GetComponent<Lock>();
-		}
-		else
-		{
-			throw new ArgumentException("Related Lock is not set.");
-		}
-	}
-	
-	// Update is called once per frame
-	void Update () {
-		castRayFromCamera();
-	}
+    private const int _cameraRayDistance = 2;
+    private Camera _fpsCamera;
 
-	void castRayFromCamera()
-	{
-		if (relatedLock == null)
-		{
-			print("Related Lock is not set.");
-			
-			return;
-		}
-		
-		if (_laserHolder == null)
-		{
-			print("Can not find a child gameObject named LaserHolder in Key object.");
-			
-			return;
-		}
-		
-		RaycastHit hit;
-		Vector3 playerForward = _fpsCamera.transform.forward;
-		Debug.DrawRay(_fpsCamera.transform.position, playerForward * _cameraRayDistance, Color.green);
+    private bool _isOn;
+
+    private Lock _lock;
+
+    // Use this for initialization
+    void Start()
+    {
+        _fpsCamera = Camera.main;
+        isOn = false;
+
+        var childTransform = transform.Find("LaserHolder");
+        if (childTransform != null)
+        {
+            _laserHolder = childTransform.gameObject;
+            _laserLineRenderer = _laserHolder.GetComponent<LineRenderer>();
+            _laserLineRenderer.enabled = isOn;
+        }
+        else
+        {
+            throw new ArgumentException("Can not find a child gameObject named LaserHolder in Key object.");
+        }
+
+        if (relatedLock != null)
+        {
+            _lock = relatedLock.GetComponent<Lock>();
+        }
+        else
+        {
+            throw new ArgumentException("Related Lock is not set.");
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        castRayFromCamera();
+    }
+
+    void castRayFromCamera()
+    {
+        if (relatedLock == null)
+        {
+            print("Related Lock is not set.");
+
+            return;
+        }
+
+        if (_laserHolder == null)
+        {
+            print("Can not find a child gameObject named LaserHolder in Key object.");
+
+            return;
+        }
+
+        RaycastHit hit;
+        Vector3 playerForward = _fpsCamera.transform.forward;
+        Debug.DrawRay(_fpsCamera.transform.position, playerForward * _cameraRayDistance, Color.green);
 
         int layerMask = 1 << LayerMask.NameToLayer("KeyLayer");
         if (Physics.Raycast(_fpsCamera.transform.position, playerForward, out hit, _cameraRayDistance, layerMask))
-		{
-          
-			if (Input.GetMouseButtonDown(0))
-			{
-				isOn = !isOn;
-			}
-			
-		}
+        {
+            if (Input.GetMouseButtonDown(0))
+            {
+                isOn = !isOn;
+            }
+        }
 
-		if (isOn)
-		{
-			ShootLaserFromTargetPosition(_laserHolder.transform.position, -1*_laserHolder.transform.forward, laserMaxLength);
-		}
-		else
-		{
-			// Laser is off, if the interactableObj is open, close.
-			if (_lock.interactState)
-			{
-				_lock.interactState = false;
-			}
-		}
-	}
-	
-	void ShootLaserFromTargetPosition( Vector3 targetPosition, Vector3 direction, float length )
-	{
-		if (relatedLock == null)
-		{
-			print("Related Lock is not set.");
-			
-			return;
-		}
-		
-		if (_laserHolder == null)
-		{
-			print("Can not find a child gameObject named LaserHolder in Key object.");
-			
-			return;
-		}
-		
-		RaycastHit raycastHit;
-		Vector3 endPosition = targetPosition + ( length * direction );
-	    Debug.DrawRay(targetPosition, direction * 100, Color.cyan);
-	    int layerMask = 1 << LayerMask.NameToLayer("LockLayer");
-        if (Physics.Raycast(targetPosition, direction, out raycastHit, laserMaxLength, layerMask)) {
-			endPosition = raycastHit.point;
-			// If interactableObj is not open and the ray hits the lock
-			if (!_lock.interactState && raycastHit.collider.gameObject == relatedLock)
-			{
-				_lock.interactState = true;
-			}
-
-			// Laser is on but we are no longer colliding with the lockObject
-			if (_lock.interactState && raycastHit.collider.gameObject != relatedLock)
-			{
-				_lock.interactState = false;
-			}
-		}
+        if (isOn)
+        {
+            ShootLaserFromTargetPosition(_laserHolder.transform.position, -1 * _laserHolder.transform.forward,
+                laserMaxLength);
+        }
         else
         {
-	        // We don't hit anything and the door is open.
-	        if (_lock.interactState)
-	        {
-		        _lock.interactState = false;
-	        }
+            // Laser is off, if the interactableObj is open, close.
+            if (_lock.interactState)
+            {
+                _lock.interactState = false;
+            }
         }
- 
-		_laserLineRenderer.SetPosition( 0, targetPosition );
-		_laserLineRenderer.SetPosition( 1, endPosition );
-	}
+    }
+
+    void ShootLaserFromTargetPosition(Vector3 targetPosition, Vector3 direction, float length)
+    {
+        if (relatedLock == null)
+        {
+            print("Related Lock is not set.");
+
+            return;
+        }
+
+        if (_laserHolder == null)
+        {
+            print("Can not find a child gameObject named LaserHolder in Key object.");
+
+            return;
+        }
+
+        RaycastHit raycastHit;
+        Vector3 endPosition = targetPosition + (length * direction);
+        Debug.DrawRay(targetPosition, direction * 100, Color.cyan);
+        int layerMask = 1 << LayerMask.NameToLayer("LockLayer");
+        if (Physics.Raycast(targetPosition, direction, out raycastHit, laserMaxLength, layerMask))
+        {
+            endPosition = raycastHit.point;
+            // If interactableObj is not open and the ray hits the lock
+            if (!_lock.interactState && raycastHit.collider.gameObject == relatedLock)
+            {
+                _lock.interactState = true;
+            }
+
+            // Laser is on but we are no longer colliding with the lockObject
+            if (_lock.interactState && raycastHit.collider.gameObject != relatedLock)
+            {
+                _lock.interactState = false;
+            }
+        }
+        else
+        {
+            // We don't hit anything and the door is open.
+            if (_lock.interactState)
+            {
+                _lock.interactState = false;
+            }
+        }
+
+        _laserLineRenderer.SetPosition(0, targetPosition);
+        _laserLineRenderer.SetPosition(1, endPosition);
+    }
 }
